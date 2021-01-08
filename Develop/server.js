d3cbd4d3bb746c8b62eb707511335a8309f13ab8
Eric M <eamenatt@gmail.com>
2021-01-08 12:45:26 -0500

    adding missing folders and file structure, added server.js file

---
+++ Develop/server.js
@@ -0,0 +1,27 @@
+//Requirements and declaring constants
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutDB", {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
+
+//Routes
+app.use(require("./routes/api-routes"));
+app.use(require("./routes/html-routes"));
+
+//Connection
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
